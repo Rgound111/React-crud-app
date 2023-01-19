41f9cb9709354baa48d7503acc19116315916541
Rohit gound <110964525+Rgound111@users.noreply.github.com>
2023-01-19 21:33:54 +0530

    Update api.jsx

--- src/service/api.jsx
+++ src/service/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-const API_URL = "https://papaya-marzipan-f944fd.netlify.app/users";
+const API_URL = "https://master--papaya-marzipan-f944fd.netlify.app/users";
 
 export const addUser = async (Data) => {
   try {
